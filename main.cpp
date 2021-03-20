f3cc29dd59a7e061367c3c24ebbc604f315d4c5c
Arvind Yadav <arvindrajayadav@gmail.com>
2021-03-20 10:49:47 +0400

    Wood to Bronze League
    Started using constants for values
    Use boost when far away from a checkpoint

--- main.cpp
+++ main.cpp
@@ -12,6 +12,20 @@ using namespace std;
 
 int main()
 {
+    //maximum speed we are allowed
+    const int MAX_THRUST = 100;
+
+    //deciding when to use boost
+    //we should at least be this far away from our next checkpoint
+    const int MIN_BOOST_DISTANCE = 1200;
+
+    //when using boost, we should also be facing our checkpoint
+    //otherwise you might boost in a different direction
+    //this is the maximum angle difference to use boost
+    const int MAX_BOOST_ANGLE = 10;
+
+    //have we used our boost already? (once per game)
+    bool usedBoost = false;
 
     // game loop
     while (1) {
@@ -33,16 +47,30 @@ int main()
         int finalThrust;
         
         //don't accelerate when we're facing away from the next checkpoint
-        //this helps us turn faster 
+        //this helps us turn faster
         if (nextCheckpointAngle < -90 || 
             nextCheckpointAngle > 90) 
             finalThrust = 0;
         else 
-            finalThrust = 100;
+            finalThrust = MAX_THRUST;
 
-        // You have to output the target position
-        // followed by the power (0 <= thrust <= 100)
-        // i.e.: "x y thrust"
-        cout << nextCheckpointX << " " << nextCheckpointY << " " << finalThrust << endl;
+        if(!usedBoost)
+        {
+            //if we're facing our target and it's far away, boost
+            if(nextCheckpointDist > MIN_BOOST_DISTANCE && nextCheckpointAngle < MAX_BOOST_ANGLE)
+            {
+                cout << nextCheckpointX << " " << nextCheckpointY << " BOOST"<< endl;
+                usedBoost = true;
+            }    
+        }
+        else
+        {
+            //normal movement
+
+            // You have to output the target position
+            // followed by the power (0 <= thrust <= 100)
+            // i.e.: "x y thrust"
+            cout << nextCheckpointX << " " << nextCheckpointY << " " << finalThrust << endl;
+        }        
     }
 }
